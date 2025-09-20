83015cbe73201198634a4dae3f3097877925b1dd
Dhiraj-4 <dhirajdipaklondhe4@gmail.com>
2025-09-21 01:59:57 +0530

    Created basic Job post schema

---
+++ src/schema/jobPostSchema.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: String, // username of the user
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+      minlength: 50,
+      maxlength: 3000,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    applyLink: {
+      type: String,
+      default: "",
+    },
+
+    companyName: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true, // createdAt & updatedAt
+  }
+);
+
+export const Job = mongoose.model("Job", jobSchema);
